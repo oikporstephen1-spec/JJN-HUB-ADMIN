507eb13a881d5375f579ca1ec54b720cd74faa8d
oikporstephen1-spec <oikporstephen1@gmail.com>
2026-06-08 14:37:23 +0300

    Update logout.js

--- js/logout.js
+++ js/logout.js
@@ -1,6 +1,23 @@
 const logoutBtn =
 document.getElementById("logoutBtn");
 
+const userName =
+document.getElementById("userName");
+
+(async () => {
+
+const { data } =
+await supabaseClient.auth.getUser();
+
+if(data.user && userName){
+
+userName.textContent =
+data.user.email;
+
+}
+
+})();
+
 if(logoutBtn){
 
 logoutBtn.addEventListener(
