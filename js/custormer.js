f1546e537d5025bd9c46e25f41c19e81229f4890
oikporstephen1-spec <oikporstephen1@gmail.com>
2026-06-08 15:44:25 +0300

    Add files via upload

---
+++ js/custormer.js
@@ -0,0 +1,112 @@
+const supabaseUrl =
+"https://aosewocqcpoffckmnfpn.supabase.co";
+
+const supabaseKey =
+"YOUR_ANON_PUBLIC_KEY";
+
+const supabase =
+window.supabase.createClient(
+supabaseUrl,
+supabaseKey
+);
+
+const form =
+document.getElementById("customerForm");
+
+const tableBody =
+document.getElementById("customerTableBody");
+
+async function loadCustomers() {
+
+const { data, error } =
+await supabase
+.from("customers")
+.select("*")
+.order("id",{ascending:false});
+
+if(error){
+console.log(error);
+return;
+}
+
+tableBody.innerHTML = "";
+
+data.forEach(customer => {
+
+tableBody.innerHTML += `
+<tr>
+<td>${customer.id}</td>
+<td>${customer.customer_name}</td>
+<td>${customer.company_name}</td>
+<td>${customer.email}</td>
+<td>${customer.phone}</td>
+<td>
+<button class="action-btn">
+View
+</button>
+</td>
+</tr>
+`;
+
+});
+
+}
+
+form.addEventListener(
+"submit",
+async (e)=>{
+
+e.preventDefault();
+
+const customer_name =
+document.getElementById(
+"customer_name"
+).value;
+
+const company_name =
+document.getElementById(
+"company_name"
+).value;
+
+const email =
+document.getElementById(
+"email"
+).value;
+
+const phone =
+document.getElementById(
+"phone"
+).value;
+
+const address =
+document.getElementById(
+"address"
+).value;
+
+const { error } =
+await supabase
+.from("customers")
+.insert([{
+customer_name,
+company_name,
+email,
+phone,
+address
+}]);
+
+if(error){
+
+alert(error.message);
+return;
+
+}
+
+alert("Customer Added");
+
+form.reset();
+
+loadCustomers();
+
+});
+
+loadCustomers();
