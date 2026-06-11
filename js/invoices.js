dbe0cc3a494db47cf010bb9ee2971ff64394e14e
oikporstephen1-spec <oikporstephen1@gmail.com>
2026-06-11 16:09:06 +0300

    Update invoices.js

--- js/invoices.js
+++ js/invoices.js
@@ -1,1 +1,140 @@
+const invoiceSupabase =
+window.supabaseClient;
 
+const tableBody =
+document.getElementById(
+"invoiceTableBody"
+);
+
+async function loadInvoices(){
+
+  const { data, error } =
+  await invoiceSupabase
+  .from("invoices")
+  .select("*")
+  .order("id",
+  {
+    ascending:false
+  });
+
+  if(error){
+
+    alert(error.message);
+
+    return;
+
+  }
+
+  tableBody.innerHTML = "";
+
+  data.forEach(invoice=>{
+
+    tableBody.innerHTML += `
+    <tr>
+
+      <td>${invoice.id}</td>
+
+      <td>
+        ${invoice.invoice_number}
+      </td>
+
+      <td>
+        ${invoice.customer_name || "Unknown"}
+      </td>
+
+      <td>
+        ₦${Number(
+          invoice.total
+        ).toLocaleString()}
+      </td>
+
+      <td>
+        ${invoice.status}
+      </td>
+
+      <td>
+
+        <button
+        class="action-btn"
+        onclick="markPaid(${invoice.id})">
+
+        Paid
+
+        </button>
+
+        <button
+        class="action-btn"
+        onclick="deleteInvoice(${invoice.id})">
+
+        Delete
+
+        </button>
+
+      </td>
+
+    </tr>
+    `;
+
+  });
+
+}
+
+async function markPaid(id){
+
+  const { error } =
+  await invoiceSupabase
+  .from("invoices")
+  .update({
+    status:"Paid"
+  })
+  .eq("id",id);
+
+  if(error){
+
+    alert(error.message);
+
+    return;
+
+  }
+
+  alert(
+    "Invoice Marked Paid"
+  );
+
+  loadInvoices();
+
+}
+
+async function deleteInvoice(id){
+
+  if(
+    !confirm(
+      "Delete Invoice?"
+    )
+  ){
+    return;
+  }
+
+  const { error } =
+  await invoiceSupabase
+  .from("invoices")
+  .delete()
+  .eq("id",id);
+
+  if(error){
+
+    alert(error.message);
+
+    return;
+
+  }
+
+  alert(
+    "Invoice Deleted"
+  );
+
+  loadInvoices();
+
+}
+
+loadInvoices();
