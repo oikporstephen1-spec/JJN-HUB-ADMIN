3c1a3d3e435a30570d674a378405fc7a780bf661
oikporstephen1-spec <oikporstephen1@gmail.com>
2026-07-07 09:08:05 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  supabaseUrl: "...",
+  supabaseAnonKey: "...",
+  companyName: "JJN HUB",
+  currency: "NGN",
+  vatRate: 0.075,
+  quotationPrefix: "QT",
+  projectPrefix: "ENG"
+};
