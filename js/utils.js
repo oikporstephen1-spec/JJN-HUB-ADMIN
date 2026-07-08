d3d18d16808db95d0b9511d326c3905c535307b4
oikporstephen1-spec <oikporstephen1@gmail.com>
2026-07-08 12:20:35 +0100

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,17 @@
+function showLoading(message = "Loading...") {
+
+    console.log(message);
+
+}
+
+function hideLoading() {
+
+    console.log("Done");
+
+}
+
+function notify(message, type = "info") {
+
+    alert(message);
+
+}
